465129e9ebe2ecde227a517f9c9afafb4fb94adf
Ulibka68 <gena68@inbox.ru>
2021-05-14 10:35:20 +0300

    добавил копирование результата

---
+++ cp-wp.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# скопировать VUE на SMF
+rm -r /mnt/f/OpenServer/domains/woodcraftsman//Themes/japanese-red-rsp/scripts/css
+rm -r /mnt/f/OpenServer/domains/woodcraftsman//Themes/japanese-red-rsp/scripts/js
+
+cp -r ./s3-vue-upload/dist/css /mnt/f/OpenServer/domains/woodcraftsman//Themes/japanese-red-rsp/scripts/css
+cp -r ./s3-vue-upload/dist/js /mnt/f/OpenServer/domains/woodcraftsman//Themes/japanese-red-rsp/scripts/js
+cp ./s3-vue-upload/dist/module-names.js /mnt/f/OpenServer/domains/woodcraftsman//Themes/japanese-red-rsp/scripts/js
--- s3-vue-upload/src/test/read-dir.mjs
+++ s3-vue-upload/src/test/read-dir.mjs
@@ -4,19 +4,19 @@ const testFolderCSS = "./dist/css";
 
 
 function main2() {
-  let resVal=' const moduleNames= {\n';
+  let resVal=' const smf_theme_url="../..";\n const smf_theme_url_scripts=smf_theme_url+"/dist"; \nconst moduleNames= {\n';
   let files=  readdirSync(testFolder);
   files.filter(value => (!value.endsWith('map'))).forEach((file) => {
     if (file.startsWith('app')){
-      resVal = `${resVal}   APP:"../../dist/js/${file}",`;
+      resVal = `${resVal}   APP:smf_theme_url_scripts+"/js/${file}",`;
     }
     else {
-      resVal = `${resVal}\n   VENDOR:"../../dist/js/${file}",`;
+      resVal = `${resVal}\n   VENDOR:smf_theme_url_scripts+"/js/${file}",`;
     }
   });
 
   files=  readdirSync(testFolderCSS);
-  resVal = `${resVal}\n   CSS:"../../dist/css/${files[0]}"`;
+  resVal = `${resVal}\n   CSS:smf_theme_url_scripts+"/css/${files[0]}"`;
   resVal = resVal+'\n}';
   console.log(resVal);
   writeFileSync('./dist/module-names.js',resVal);
